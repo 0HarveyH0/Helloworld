a459e5c99a2625edbb9002e41425790bc28c5b02
ESCG\521013 <521013@chrome.escg.ac.uk>
2021-09-23 15:01:35 +0100

    Updated Lists

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Hello_World
 {
     class Program
     {
+        static int age;
+        static string message = "My age is ";
+        static double Height;
+        static string heightMessage = " and my height in meters is ";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            List<int> numberList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for(int i = 0; i < numberList.Count; i++)
+            {
+                Console.WriteLine(numberList[i]);
+            }
+
+            List<string> wordList = new List<string>() { "joe", "mama"};
+            for (int i = 0; i < wordList.Count; i++)
+            {
+                Console.WriteLine(wordList[i]);
+            }
+
+
+
+            age = 16;
+            Height = 1.7;
+            Console.WriteLine(message + age + heightMessage + Height);
+            Console.ReadKey();
         }
     }
+
+
 }
